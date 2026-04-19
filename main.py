f150f6c01ed335884b48a2d2321cbeee22308811
KeyofDestiny17 <destinykey17@gmail.com>
2026-04-19 11:44:58 -0500

    Made a clock that runs at 60 frames

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
 if __name__ == "__main__":
     main()
