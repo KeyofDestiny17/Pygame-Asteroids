46fc95af2a304d4313728345727edae0eb295d01
KeyofDestiny17 <destinykey17@gmail.com>
2026-04-26 12:05:52 -0500

    added updatable and drawable groups and updated the game loop to use them

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -17,8 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
